8ca329b22803af1a42f2925b53e0726079a9b35c
Như Ngọc <nhnngoc05@gmail.com>
2026-05-22 20:57:14 +0700

    fix incorrect name einvoice

--- account_move_einvoice/__manifest__.py
+++ account_move_einvoice/__manifest__.py
@@ -5,8 +5,8 @@
     'summary': 'Tích hợp nút phát hành Hóa đơn điện tử mô phỏng',
     'depends': ['account'], # Kế thừa từ module Kế toán (Invoicing)
     'data': [
-        'security/einvoice_security.xml'
-        'views/account_move_view.xml',
+        'security/einvoice_security.xml',
+        'views/account_move_invoice_view.xml'
     ],
     'installable': True,
     'application': False,
